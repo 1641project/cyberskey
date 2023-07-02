f42e85abdc7f0d92bef78c2fb0c5038e613deaea
cutls <web-pro@cutls.com>
2023-07-02 12:52:48 +0000

    fix conflict

--- packages/frontend/src/pages/auth.vue
+++ packages/frontend/src/pages/auth.vue
@@ -1,31 +1,14 @@
 <template>
-<MkStickyContainer>
-	<template #header><MkPageHeader :actions="headerActions" :tabs="headerTabs"/></template>
-	<MkSpacer :contentMax="500">
-		<div v-if="state == 'fetch-session-error'">
-			<p>{{ i18n.ts.somethingHappened }}</p>
-		</div>
-		<div v-else-if="$i && !session">
-			<MkLoading/>
-		</div>
-		<div v-else-if="$i && session">
-			<XForm
-				v-if="state == 'waiting'"
-				class="form"
-				:session="session"
-				@denied="state = 'denied'"
-				@accepted="accepted"
-			/>
-			<div v-if="state == 'denied'">
-				<h1>{{ i18n.ts._auth.denied }}</h1>
+	<MkStickyContainer>
+		<template #header>
+			<MkPageHeader :actions="headerActions" :tabs="headerTabs" />
+		</template>
+		<MkSpacer :contentMax="500">
+			<div v-if="state == 'fetch-session-error'">
+				<p>{{ i18n.ts.somethingHappened }}</p>
 			</div>
-			<div v-if="state == 'accepted' && session">
-				<h1>{{ session.app.isAuthorized ? i18n.t('already-authorized') : i18n.ts.allowed }}</h1>
-				<p v-if="session.app.callbackUrl">
-					{{ i18n.ts._auth.callback }}
-					<MkEllipsis/>
-				</p>
-				<p v-if="!session.app.callbackUrl">{{ i18n.ts._auth.pleaseGoBack }}</p>
+			<div v-else-if="$i && !session">
+				<MkLoading />
 			</div>
 			<div v-else-if="$i && session">
 				<XForm v-if="state == 'waiting'" class="form" :session="session" @denied="state = 'denied'"
@@ -34,17 +17,32 @@
 					<h1>{{ i18n.ts._auth.denied }}</h1>
 				</div>
 				<div v-if="state == 'accepted' && session">
-					<h1>{{ session.app.isAuthorized ? $t('already-authorized') : i18n.ts.allowed }}</h1>
+					<h1>{{ session.app.isAuthorized ? i18n.t('already-authorized') : i18n.ts.allowed }}</h1>
 					<p v-if="session.app.callbackUrl">
 						{{ i18n.ts._auth.callback }}
 						<MkEllipsis />
 					</p>
 					<p v-if="!session.app.callbackUrl">{{ i18n.ts._auth.pleaseGoBack }}</p>
 				</div>
-			</div>
-			<div v-else>
-				<p :class="$style.loginMessage">{{ i18n.ts._auth.pleaseLogin }}</p>
-				<MkSignin @login="onLogin" />
+				<div v-else-if="$i && session">
+					<XForm v-if="state == 'waiting'" class="form" :session="session" @denied="state = 'denied'"
+						@accepted="accepted" />
+					<div v-if="state == 'denied'">
+						<h1>{{ i18n.ts._auth.denied }}</h1>
+					</div>
+					<div v-if="state == 'accepted' && session">
+						<h1>{{ session.app.isAuthorized ? $t('already-authorized') : i18n.ts.allowed }}</h1>
+						<p v-if="session.app.callbackUrl">
+							{{ i18n.ts._auth.callback }}
+							<MkEllipsis />
+						</p>
+						<p v-if="!session.app.callbackUrl">{{ i18n.ts._auth.pleaseGoBack }}</p>
+					</div>
+				</div>
+				<div v-else>
+					<p :class="$style.loginMessage">{{ i18n.ts._auth.pleaseLogin }}</p>
+					<MkSignin @login="onLogin" />
+				</div>
 			</div>
 		</MkSpacer>
 	</MkStickyContainer>
